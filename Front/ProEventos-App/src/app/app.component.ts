f0a09cbc5c8fecf8678994fb42eebfad64e3753c
nicolas.carvalho <nicolas.carvalho@cgm.fortaleza.ce.gov.br>
2024-07-18 13:56:10 -0300

    Table de eventos

--- Front/ProEventos-App/src/app/app.component.ts
+++ Front/ProEventos-App/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { EventosComponent } from "./eventos/eventos.component";
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, EventosComponent, PalestrantesComponent],
+  imports: [CommonModule, RouterOutlet, EventosComponent, PalestrantesComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,17 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { error } from 'console';
+import { FormsModule } from '@angular/forms';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @Component({
   selector: 'app-eventos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CollapseModule, FormsModule],
   templateUrl: './eventos.component.html',
-  styleUrl: './eventos.component.css',
+  styleUrls: ['./eventos.component.css'],
 })
 export class EventosComponent {
-  public eventos: any;
+  public eventos: any[] = [];
+  public eventosFiltrados: any[] = [];
+  mostrarImagem: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -21,8 +34,23 @@ export class EventosComponent {
 
   public getEventos(): void {
     this.http.get('http://localhost:5186/Eventos').subscribe(
-      (response) => (this.eventos = response),
-      (error) => console.log(error)
+      response => {
+        this.eventos = response as any[];
+        this.eventosFiltrados = this.eventos;
+      },
+      error => console.log(error)
+    );
+  }
+
+  alterarImagem() {
+    this.mostrarImagem = !this.mostrarImagem;
+  }
+
+  filtrarEventos(filtrarPor: string): any[] {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+                       evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
     );
   }
 }
